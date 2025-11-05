566e1684ceba1aa349a2100422fabcbffa6316a2
AlanLangelan <alanlangelan.2016@gmail.com>
2025-11-05 08:50:47 -0300

    ¨EstructuraInicial¨

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Provider } from 'react-redux'
+import { store } from './store/store'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import NoEncontrado from './components/NoEncontrado'
+import ProtectedRoute from './components/ProtectedRoute'
+
+
+function App() {
+  return(
+    <>
+    <Provider store={store}>
+
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/registro" element={<Registro />} />
+      <Route path="/dashboard" element={<ProtectedRoute />}>
+      <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+
+      <Route path="*" element={<NoEncontrado />} />
+
+      </Routes>
+      </BrowserRouter>
+
+    </Provider>
+    </>
+  )
+ }
+
+export default App
---
+++ src/i18n/index.js
@@ -0,0 +1,24 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './locales/en/translation.json';
+import es from './locales/es/translation.json';
+
+const idiomaActual = localStorage.getItem('idioma') || 'en';
+i18next.use(initReactI18next).init({
+    resources: {
+        en: { translation: en },
+        es: { translation: es },
+    },
+    lng: idiomaActual, // idioma por defecto
+    fallbackLng: 'es', // idioma de respaldo
+    interpolation: {
+        escapeValue: false, // react ya se encarga del escape
+    },
+    });
+    i18next.on('languageChanged', (lng) => {
+        localStorage.setItem('idioma', lng);
+        });
+
+   
+
+export default i18next;
